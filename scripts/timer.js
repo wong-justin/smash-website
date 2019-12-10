2a1f5a58baf98a687983711064a755b575b5fe75
Justin Wong <jkwongfl@yahoo.com>
2019-12-09 23:18:41 -0500

    reversed timer from countdown to count up

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,17 +1,18 @@
-var end = new Date("Dec 7, 2018 00:00:00").getTime();
-
+var begin = new Date("Dec 7, 2018 00:00:00").getTime();
 
 var x = setInterval(function () {    // update every so often
 
     var now = new Date().getTime();
-    var diff = end - now;
+    var diff = now - begin;
 
-    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+    var days = Math.floor((diff % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
     var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     // display result in paragraph element since it has id "timer"
+    document.getElementById("timery").innerHTML = years;
     document.getElementById("timerd").innerHTML = days;
     document.getElementById("timerh").innerHTML = hours;
     document.getElementById("timerm").innerHTML = minutes;
